abb2eb9c6be0edb71548f0d1d154574ff0cd845b
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-11-28 20:52:35 +0300

    eslint-settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.module.scss";
+import React from 'react';
+import './App.module.scss';
+import Find from './component/Find';
 
 function App() {
   return (
     <div>
-      <header></header>
-      <main></main>
-      <footer></footer>
+      <header>
+        <h1>Система брони</h1>
+      </header>
+      <main>
+        <Find />
+        <div>Меню</div>
+        <div>Забронировать</div>
+      </main>
+      <footer>все зарегано</footer>
     </div>
   );
 }
